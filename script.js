32c470d4e79ad201bcdbba434f18bac72ab9c411
erin cherney <erin.t.cherney@gmail.com>
2016-06-09 13:35:14 -0400

    pseudocode

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
   console.log("connected")
+
+
+  //there will be an array of questions and an array of answers, with matching indexes
+
+  //the game start with the gif of the first part of the video, then alerts you that the game is starting
+
+  //your id will show up at box 3 or so, with shia at box 1.
+
+  //A math problem will show up in header 2. You can type in the answer and press enter to compare
+
+  //if your answer matches the answer in the array, you move forward
+
+  //if not, it sets the inbox blank and shows up a red x next to your answer (show)
+
+  //shia moves on a timer, fades in and out of each box.
+  //(while game is playing, move shia +1 in table.)
+  //check for dead every time shia moves.
+  //check for close each time shia moves (if yourspot-shiaspot < 2, unhide the text)
+
+
 });
