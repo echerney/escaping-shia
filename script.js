edb5d12bd732c3f1b9edb9ce106f0d3aa8b37746
erin cherney <erin.t.cherney@gmail.com>
2016-06-09 16:02:33 -0400

    more pseudocode, got the player moving

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
   console.log("connected")
 
-
+qArray = ['1', '2', '3', '4', '5', '6', '7']
+aArray = ['1', '2', '3', '4', '5', '6', '7']
 
   //there will be an array of questions and an array of answers, with matching indexes
 
@@ -9,7 +10,36 @@ $(document).ready(function(){
 
   //your id will show up at box 3 or so, with shia at box 1.
 
+  // while class = playing... keep going with the game
+
+  // make conditions for when the game will be over, and use those to turn off playing
   //A math problem will show up in header 2. You can type in the answer and press enter to compare
+function showQuestion() {
+  var qNum = Math.floor(Math.random() * qArray.length);
+  $('#question').text(qArray[qNum]);
+}
+
+function compareAnswer() {
+  $('button').click(function(){
+    if($('input').val() == $('#question').text()){
+      movePlayer();
+    } else {
+      console.log("wrong");
+    }
+  })
+};
+
+function movePlayer() {
+  var prevID = parseInt($('.player').attr('id').replace('b',''));
+  var prevPlayer = "#b" + prevID
+  var nextID = "#b" + (prevID + 1);
+  $(nextID).addClass('player');
+  setTimeout($(prevPlayer).removeClass('player'), 30);
+}
+
+showQuestion();
+compareAnswer();
+
 
   //if your answer matches the answer in the array, you move forward
 
@@ -19,16 +49,16 @@ $(document).ready(function(){
   //(while game is playing, move shia +1 in table.)
   //check for dead every time shia moves.
   //check for close each time shia moves (if yourspot-shiaspot < 2, unhide the text)
-  function moveShia() {
-    var prevID = parseInt($('.shia').attr('id').replace('b',''));
-    var prevShia = "#b" + prevID
-    var nextID = "#b" + (prevID + 1);
-    $(nextID).addClass('shia');
-    setTimeout($(prevShia).removeClass('shia'), 30);
-  };
+  // function moveShia() {
+  //   var prevID = parseInt($('.shia').attr('id').replace('b',''));
+  //   var prevShia = "#b" + prevID
+  //   var nextID = "#b" + (prevID + 1);
+  //   $(nextID).addClass('shia');
+  //   setTimeout($(prevShia).removeClass('shia'), 30);
+  // };
 
 
-window.setInterval(moveShia, 5000);
+// window.setInterval(moveShia, 5000);
 
 
 });
