ffdc149ff072ea7ff573a76597bae81b939545fd
erin cherney <erin.t.cherney@gmail.com>
2016-06-10 17:44:59 -0400

    testing

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ $(document).ready(function(){
   console.log("connected")
 
   //there will be an array of questions and an array of answers, with matching indexes
-var qArray = ['1', '2', '3', '4', '5', '6', '7']
-var aArray = ['1', '2', '3', '4', '5', '6', '7']
+var qArray = ['17 + 5', '3 * 5', '16 + 23', '54 / 6', '12 + 54',
+'57 - 35', '22 - 8', '72 / 9', '5 * 5 * 3', '56 / 2',
+'18 - 9', '26 - 14', '45 / 15', '26 / 2', '24 / 8',
+'70 / 5', '58-15', '27-15', '6 * 2 * 2', '60 / 5',
+'5 * 6', '5.5 * 4', '17 - 8', '3 * 13', '45 / 9']
+var aArray = ['22', '15', '39', '9', '66',
+'22', '14', '8', '75', '28',
+'9', '12', '3', '13', '3',
+'14', '43', '12', '24', '12',
+'30', '22', '9', '39', '5']
 
   //the game start with the gif of the first part of the video, then alerts you that the game is starting
 
 
   //A math problem will show up in header 2. You can type in the answer and press enter to compare
 function showQuestion() {
-  var qNum = Math.floor(Math.random() * qArray.length);
+  qNum = Math.floor(Math.random() * qArray.length);
   $('#question').text(qArray[qNum]);
 }
 
@@ -18,15 +26,18 @@ function showQuestion() {
   //if not, it sets the inbox blank and shows up a red x next to your answer (show)
   //checks for wins each time you progress.
 function compareAnswer() {
-  $('button').click(function(){
-    if($('input').val() == $('#question').text()){
-      movePlayer();
-      showQuestion();
-      $("input").val('')
-    } else {
-      console.log("wrong")
-      $("#wrong").fadeIn(300).fadeOut(300);
-      $("input").val('')
+  $('input').on('keypress', function(e){
+    var keycode = (e.keyCode);
+    if(keycode == '13'){
+      if($('input').val() == aArray[qNum]) {
+        movePlayer();
+        showQuestion();
+        $("input").val('')
+      } else {
+        console.log("wrong")
+        $("#wrong").fadeIn(300).fadeOut(300);
+        $("input").val('')
+      }
     }
     checkForWin();
   })
@@ -68,7 +79,7 @@ function endGame() {
 };
 
 function shiaWins() {
-
+  alert("You have just been eaten by Shia Lebouf.")
 }
 
 function bearTrap() {
@@ -104,17 +115,17 @@ function bearTrap() {
   };
 
 setTimeout(function() {
-  $("#video").hide();
+  $("#video").fadeOut();
   $('#game').addClass('playing');
 }, 18000);
 
 setTimeout(function() {
   if ($('#game').hasClass('playing') == true) {
-    window.setInterval(moveShia, 3500);
+    window.setInterval(moveShia, 5500);
     showQuestion();
     compareAnswer();
   } else {
     console.log("uhoh");
   }
-}, 19000);
+}, 21000);
 });
