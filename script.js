a9371883f356341ab0b29542ad99c8a79cedb6a7
erin cherney <erin.t.cherney@gmail.com>
2016-06-10 10:53:58 -0400

    changed layout

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 $(document).ready(function(){
   console.log("connected")
 
+  //there will be an array of questions and an array of answers, with matching indexes
 var qArray = ['1', '2', '3', '4', '5', '6', '7']
 var aArray = ['1', '2', '3', '4', '5', '6', '7']
 
-  //there will be an array of questions and an array of answers, with matching indexes
-
   //the game start with the gif of the first part of the video, then alerts you that the game is starting
 
-  //your id will show up at box 3 or so, with shia at box 1.
 
-  // while class = playing... keep going with the game
-
-  // make conditions for when the game will be over, and use those to turn off playing
   //A math problem will show up in header 2. You can type in the answer and press enter to compare
 function showQuestion() {
   var qNum = Math.floor(Math.random() * qArray.length);
@@ -21,6 +16,7 @@ function showQuestion() {
 
   //if your answer matches the answer in the array, you move forward
   //if not, it sets the inbox blank and shows up a red x next to your answer (show)
+  //checks for wins each time you progress.
 function compareAnswer() {
   $('button').click(function(){
     if($('input').val() == $('#question').text()){
@@ -37,33 +33,43 @@ function compareAnswer() {
 };
 
 function movePlayer() {
-  var prevID = parseInt($('.player').attr('id').replace('b',''));
-  var prevPlayer = "#b" + prevID
-  var nextID = "#b" + (prevID + 1);
+  var prevPos = parseInt($('.player').attr('id').replace('b',''));
+  var prevID = "#b" + prevPos
+  var nextID = "#b" + (prevPos + 1);
   $(nextID).addClass('player');
-  setTimeout($(prevPlayer).removeClass('player'), 30);
+  setTimeout($(prevID).removeClass('player'), 30);
 }
 
 
 function checkForWin() {
   var wheresShia = parseInt($('.shia').attr('id').replace('b',''));
   var whereamI = parseInt($('.player').attr('id').replace('b',''));
-  // while (whereamI - wheresShia <= 1) {
-  //   $("#warning").fadeIn().fadeOut().fadeIn().fadeOut()
-  // };
   if (whereamI == wheresShia) {
     alert("Looks like you've been eaten by Shia LeBouf.");
+    endGame();
   } else if (whereamI == 16) {
     alert("You've escaped Actual Cannibal Shia Lebouf!")
+    endGame();
+  } else if (whereamI - wheresShia <= 1) {
+    $("#warning").fadeIn();
+    $("#maze").addClass("animated shake");
   } else {
-    console.log('still going!');
-  }
-}
+    $("#warning").fadeOut();
+    $("#maze").removeClass("animated shake");
+  };
+};
+
+function endGame() {
+  $('.shia').removeClass('shia');
+  $('.player').removeClass('player');
+  $('#game').removeClass('playing');
+  //show the end video?
+};
 
 
-//   //shia moves on a timer, fades in and out of each box.
-//   //check for dead every time shia moves.
-//   //check for close each time shia moves (if yourspot-shiaspot < 2, unhide the text)
+
+// shia moves on a timer, fades in and out of each box.
+// Also checks position to give warning or end game
   function moveShia() {
     var prevID = parseInt($('.shia').attr('id').replace('b',''));
     var prevShia = "#b" + prevID
@@ -73,8 +79,13 @@ function checkForWin() {
     checkForWin();
   };
 
-
+if ($('#game').hasClass('playing')== true) {
   window.setInterval(moveShia, 5000);
   showQuestion();
   compareAnswer();
+} else {
+  console.log("done");
+
+}
+
 });
